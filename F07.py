c38f8024630625302ffa7e498132cf40a001b20b
Naufal-Nalendra <103405240+Naufal-Nalendra@users.noreply.github.com>
2022-04-19 13:19:00 +0700

    Update F07.py

--- F07.py
+++ F07.py
@@ -62,54 +62,6 @@ def sort_game(list,key,scheme="+"):
     else:
         error_msg = 'Skema sorting tidak valid!'
         return error_msg
-
-def parse(file):
-
-    # KAMUS LOKAL
-    # file          : str                   - nama + path file yang hendak di-parse
-    # f             : file                  - file yang di-parse
-    # raw_data      : str                   - data mentah file
-    # cols, row     : int                   - jumlah kolom dan baris
-    # col, row, j   : int                   - index
-    # i             : char/int              - index
-    # data          : list of lists of str  - data hasil parsing
-
-    # ALGORITMA
-
-    # BUKA FILE, SIMPAN DI STRING raw_data
-    f = open(file,"r")
-    raw_data = f.read()
-    f.close()
-
-    # MENGHITUNG JUMLAH BARIS DAN KOLOM
-    cols = 1
-    rows = 1
-
-    for i in raw_data:
-        if i == ";":
-            cols += 1
-        if i == "\n":
-            rows += 1
-            cols = 1
-    
-    # INISIALISASI LIST OF LIST OF STR data 
-    data = [["" for j in range(cols)] for i in range(rows)]
-
-    # SIMPAN DATA DI LIST OF LIST OF STR data
-    col = 0
-    row = 0
-
-    for i in raw_data:
-        if (i != ";") and (i != "\n"):
-            data[row][col] += i
-        elif (i == ";"):
-            col += 1
-        else:
-            row += 1
-            col = 0 
-
-    # KEMBALIKAN LIST OF LIST OF STR data
-    return data
     
 # Main Function
 def list_game_toko():
