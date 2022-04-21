fe7fe86f4a31cf7d93b8fd04d57b98c584578f0f
Cammelofrost <102664927+Cammelofrost@users.noreply.github.com>
2022-04-21 22:27:55 +0700

    Add files via upload

---
+++ F09.py
@@ -0,0 +1,14 @@
+# import functions module
+from components.binomo import *
+from components.csv import *
+from datetime import datetime
+
+def list_game(files):
+    if csv_checker(files):
+        print_table(files)
+    else : 
+        print("Maaf, kamu belum membeli game. Ketik perintah beli_game untuk beli.")
+list_game("'components\game.csv'")
+        
+
+
